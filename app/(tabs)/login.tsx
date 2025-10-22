63709d667f84b5c951b07cc1fc38734fd9f6b2b4
Joao Pedro Ferreira Da Silva - FATEC - Cuiaba/MT <00587703@alunosenai.mt>
2025-10-21 21:17:20 -0400

    Criando a tela de login

---
+++ app/(tabs)/login.tsx
@@ -0,0 +1,15 @@
+import { View, StyleSheet } from 'react-native';
+
+export default function Login(){
+    return(
+        <View>
+            <Text>
+                Só Alegria!
+            </Text>
+        </View>
+    )
+}
+
+const style = StyleSheet.create({
+    
+});
